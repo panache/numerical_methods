ba6cd8f2812893eec1188c1d16ee20ab327c0c81
Akash Vaswani <avaswani@uwaterloo.ca>
2011-10-30 17:51:15 -0400

    question 4 is done!!# Please enter the commit message for your changes. Lines starting

---
+++ integral_func.m
@@ -0,0 +1,8 @@
+function [ f ] = integral_func( x )
+%INTEGRAL_FUNC Summary of this function goes here
+%   Detailed explanation goes here
+
+f = 5000/(30 * 10^6 * (1 - 0.1*x - 0.0005*(x^2)) * 2 * e^(-0.1*x));
+
+end
+
